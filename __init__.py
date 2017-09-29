3c5595b3eef4e3fb6ac12b61f7db897b55581799
mohaibma <mohaibma@yahoo.com>
2017-09-29 16:58:33 +0100

    modifications

--- __init__.py
+++ __init__.py
@@ -10,6 +10,11 @@ def homepage():
 @app.route('/room_rate')
 def room_rate():
     return render_template("room_rate.html")
+	
+@app.route('/about_us')
+def about_us():
+    return render_template("about_us.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+	#app.run(debug=False)
